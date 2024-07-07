096f16d2414a9a21020afcb43dc1975a9ded63fc
ayeshaimran4 <ayeshaimransh@gmail.com>
2024-07-07 15:15:05 +0500

    main

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import { Outlet } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  
+
+  return (
+   <>
+   <nav> nav bar</nav> 
+   <Outlet/>
+   </>
+  )
+}
+
+export default App;
